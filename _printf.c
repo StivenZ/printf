c7db405c92d896fae82f05fb4d77dc3eb756ada2
Stiven Espinosa <1806@holbertonschool.com>
2020-07-22 05:18:57 +0000

    Fixed %d%d condition

--- _printf.c
+++ _printf.c
@@ -14,36 +14,35 @@ int _printf(const char *format, ...)
 
 	va_start(current, format);
 	i = sum = counter = 0;
-
-	while (format != NULL && format[i] != '\0')
+while (format != NULL && format[i] != '\0')
+{
+	while (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'c'
+		|| format[i + 1] == 's' || format[i + 1] == 'i'))
 	{
-		if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'c'
-			|| format[i + 1] == 's' || format[i + 1] == 'i'))
-		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					sum = sum + c_funct(va_arg(current, int));
-					break;
-				case 's':
-					sum = sum + s_funct(va_arg(current, char *));
-					break;
-				case 'd':
-					sum = sum + d_funct(va_arg(current, int));
-					break;
-				case 'i':
-					sum = sum + d_funct(va_arg(current, int));
-					break;
-			}
-			i++;
-		}
-		if (format[i + 1] == '%' && format[i] == '%')
-			i++;
-		my_putchar(format[i]);
 		i++;
-		counter++;
+		switch (format[i])
+		{
+			case 'c':
+				sum = sum + c_funct(va_arg(current, int));
+				break;
+			case 's':
+				sum = sum + s_funct(va_arg(current, char *));
+				break;
+			case 'd':
+				sum = sum + d_funct(va_arg(current, int));
+				break;
+			case 'i':
+				sum = sum + d_funct(va_arg(current, int));
+				break;
+		}
+		i++;
 	}
+	if (format[i + 1] == '%' && format[i] == '%')
+		i++;
+	my_putchar(format[i]);
+	i++;
+	counter++;
+}
 	va_end(current);
 	return (sum + counter);
 }
