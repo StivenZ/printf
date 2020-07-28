cf7bb5c0d0e1c2e1ac1e823673805c29f9fb5b7b
santizabe <santizabe@gmail.com>
2020-07-28 01:13:45 +0000

    Task done

---
+++ holberton.h
@@ -0,0 +1,24 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int print_str(char *str);
+
+int _putchar(char c);
+
+int c_funct(char c);
+
+int s_funct(char *s);
+
+int print_Int(int i);
+
+int _printf(const char *format, ...);
+
+int d_funct(int n);
+
+int my_putchar(char c);
+
+#endif /* _HOLBERTON_H_ */
