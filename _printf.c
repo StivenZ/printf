e928f097e2a01c7c567c1b59730c2e9edd6516a6
Stiven Espinosa <1806@holbertonschool.com>
2020-07-28 02:58:14 +0000

    Adding switch case function

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+/**
+ * chofunct - chooses which function applies according to the case
+ * @c: character to compare with
+ * Return: number of characters printed.
+*/
+int chofunct(char c, va_list current)
+{
+	switch (c)
+	{
+		case 'c':
+			return (c_funct(va_arg(current, int)));
+		case 's':
+			return (s_funct(va_arg(current, char *)));
+		case 'd':
+			return (d_funct(va_arg(current, int)));
+		case 'i':
+			return (d_funct(va_arg(current, int)));
+	}
+	return (0);
+}
+/**
+ * _printf - prints according to a format
+ * @format: format specifier
+ * Return: number of bytes printed
+*/
+int _printf(const char *format, ...)
+{
+	va_list current;
+	int i, sum, counter;
+
+	if (format == NULL || format == '\0')
+		return (-1);
+
+	va_start(current, format);
+	i = sum = counter = 0;
+
+	while (format[i] != '\0')
+	{
+		while (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'c'
+			|| format[i + 1] == 's' || format[i + 1] == 'i'))
+		{
+			i++;
+			sum = sum + chofunct(format[i], current);
+			i++;
+		}
+
+		if (format[i + 1] == '%' && format[i] == '%')
+			i++;
+
+		my_putchar(format[i]);
+		i++;
+		counter++;
+	}
+	va_end(current);
+	return (sum + counter);
+}
---
+++ holberton.h
@@ -0,0 +1,18 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int c_funct(char c);
+
+int s_funct(char *s);
+
+int _printf(const char *format, ...);
+
+int d_funct(int n);
+
+int my_putchar(char c);
+
+#endif /* _HOLBERTON_H_ */
